1ad0e7f758aa68c3cec081ac30aeb2768c2c5fec
Hannah MacDonald <hannah.e.macdonald@gov.bc.ca>
2026-08-20 11:27:46 -0600

    fix: migration

--- src/lib/server/db/migrations/0018_steady_bullseye.sql
+++ src/lib/server/db/migrations/0018_steady_bullseye.sql
@@ -1,2 +1,3 @@
+TRUNCATE TABLE `account`; --> statement-breakpoint
 ALTER TABLE `account` ADD `issuer` text NOT NULL;--> statement-breakpoint
 CREATE UNIQUE INDEX `account_issuer_accountId_uidx` ON `account` (`issuer`,`account_id`);
